dbc54c2dc246f6fa33d11687cb8f94583040d1d1
BrucceVT <bvillena2000@gmail.com>
2024-06-20 16:59:43 -0500

    Add axios and material tailwind configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './main.css'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import Root from './router/root.tsx'
-import Home from './pages/Home.tsx'
-import { Provider } from 'react-redux'
-import store from './store/index.ts'
-import PrivateRoute from './components/PrivateRoute.tsx'
-import Login from './pages/Login.tsx'
-import Register from './pages/Register.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./router/root.tsx";
+import Home from "./pages/Home.tsx";
+import { Provider } from "react-redux";
+import store from "./store/index.ts";
+import PrivateRoute from "./components/PrivateRoute.tsx";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
+
+import { ThemeProvider } from "@material-tailwind/react";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <PrivateRoute>
-      <Root/>
-    </PrivateRoute>,
+    element: (
+      <PrivateRoute>
+        <Root />
+      </PrivateRoute>
+    ),
     children: [
       {
-        path: '',
-        element: <Home/>
-      }
-    ]
+        path: "",
+        element: <Home />,
+      },
+    ],
   },
   {
-    path: 'login',
-    element: <Login/>
+    path: "login",
+    element: <Login />,
   },
   {
-    path: 'register',
-    element: <Register/>
+    path: "register",
+    element: <Register />,
   },
   {
-    path: '*',
-    element: <Navigate to="/"/>
-  }
-])
+    path: "*",
+    element: <Navigate to="/" />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
